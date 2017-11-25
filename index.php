94fd25871f529aec1c2bedbe233f49d963d17f60
David J. Malan <>
2017-11-25 05:25:16 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+function adminer_object() {
+    // required to run any plugin
+    include_once "./plugins/plugin.php";
+    
+    // autoloader
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
+    
+    $plugins = array(
+        // specify enabled plugins here
+        new AdminerLogin50(),
+        new AdminerLoginSqlite,
+        new AdminerTheme("default-blue"),
+    );
+    
+    /* It is possible to combine customization and plugins:
+    class AdminerCustomization extends AdminerPlugin {
+    }
+    return new AdminerCustomization($plugins);
+    */
+    
+    return new AdminerPlugin($plugins);
+}
+
+// include original Adminer or Adminer Editor
+include "./adminer-4.3.1.php";
+?>
